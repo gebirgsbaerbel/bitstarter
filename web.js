7987d5da66f7890fb3cae12cd52bab8302b9e707
Ubuntu <ubuntu@ip-10-35-159-34.eu-west-1.compute.internal>
2013-08-21 09:10:38 +0000

    blub

--- web.js
+++ web.js
@@ -3,12 +3,11 @@ var app = express();
 app.use(express.logger());
 
 app.get('/', function(request, response) {
-  fs.readFileSync('index.html', function(err, data) {
-    response.send(data);
-  }
+  var data = fs.readFileSync('index.html');
+  response.send(data.toString());
 });
 
-var port = process.env.PORT || 5000;
+var port = process.env.PORT || 8080;
 app.listen(port, function() {
   console.log("Listening on " + port);
 });
